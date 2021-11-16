b9e80e83249902499abaa26b70c97c21cea8286b
JEBenitezSan <joseestebansanchezbenitez9514@gmail.com>
2021-11-15 19:41:08 -0600

    agregue mas mensajes

--- envios.php
+++ envios.php
@@ -1,8 +1,20 @@
 <?php
-echo"Hola Soy PHP"
-echo"Hola Soy PHP"
-echo"Estoy Actualizando  datos desde commit a una version anterior"
-echo"Estoy Actualizando  datos desde commit a una version anterior"
-echo"Estoy Actualizando  datos desde commit a una version anterior"
+echo"Hola Soy PHP";
+echo"Hola Soy PHP";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+
+echo"Hola Soy PHP";
+echo"Hola Soy PHP";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+
+echo"Hola Soy PHP";
+echo"Hola Soy PHP";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
+echo"Estoy Actualizando  datos desde commit a una version anterior";
 
 ?>
